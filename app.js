06ac5036b2182373c6c2bf5d2b5fe98caf4b5c9c
Rumeli Paul <rumelipaul@gmail.com>
2023-03-10 20:52:54 +0100

    Fixed bug with default artist, refactored code

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
-const axios = require("axios");
-const artistsJson = require("./data/artists.json");
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
-const readline = require("readline").createInterface({
+const axios = require("axios");    // require axios to get data as Promise from API endpoint
+const artistsJson = require("./data/artists.json"); // require the default artists.json from dictionary
+const createCsvWriter = require("csv-writer").createObjectCsvWriter; // require csv-writer to write to csv files
+const readline = require("readline").createInterface({    // require readline to process user inputs
   input: process.stdin,
   output: process.stdout,
 });
 const base_url = process.env.LAST_FM_URL;
 const api_key = process.env.API_KEY;
 
-//Take Artist name as user input and also as the filename input for csv
+//Take Artist name as user input and also as the filename input for csv, process the results into the csv
 readline.question(
   `Enter the name of the artist (also the filename for CSV):`,
   (name) => {
@@ -27,30 +27,14 @@ readline.question(
       });
     axios
       .get(`${base_url}&artist=${artist_name}&api_key=${api_key}&format=json`)
-      .then((response) => {
-       
-       
+      .then((response) => {     
 
         //Get the relevant Artists data from the response object returned by API method
         let data = response.data.results.artistmatches.artist;
         if(data.length === 0){
-            const randArtist = artistsJson[Math.floor(Math.random()*artistsJson.length)];
-            console.log(randArtist);
-            const defaultDataToWrite = 
-                {
-                    name: randArtist.name,
-                    mbid: randArtist.mbid,
-                    url: randArtist.url,
-                    image_small: randArtist.image.find((item) => (item.size === "small"))["#text"],
-                    image: randArtist.image.find((item1) => (item1.size === "large"))["#text"],
-                  }
-                  csvWriter.writeRecords(defaultDataToWrite).then(() => {
-                    console.log("...Done");
-                  });
-            
-            
+            data = [artistsJson[Math.floor(Math.random()*artistsJson.length)]];          
         }
-        else {
+        
             const dataToWrite = data.map((element) => ({
                 name: element.name,
                 mbid: element.mbid,
@@ -59,13 +43,11 @@ readline.question(
                 image: element.image.find((item1) => (item1.size === "large"))["#text"],
               }));
       
-              console.log(dataToWrite);
-      
               //write to the csv file
               csvWriter.writeRecords(dataToWrite).then(() => {
                 console.log("...Done");
               });
-        }
+        
         //create object to write to csv file
         
       })
